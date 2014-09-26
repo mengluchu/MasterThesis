7bfee9705588d92bb9a40e74bee240a16e93c595
ChristopherStephan <c.stephan@wwu.de>
2014-09-26 13:16:43 -0300

    updates

--- feasability_proof.R
+++ feasability_proof.R
@@ -1,9 +1,11 @@
 library(rworldmap)
 library(ggmap)
 library(sp)
+library(bfast)
 
 # file named deter.csv with samples of time series related to DETER evaluation
 deter = read.csv("/home/christopher/Documents/Master Thesis/Orientation/deter.csv")
+
 # subset of area of Mato Grosso with bbox of -61,6325, -18,0418, -50,2244, -7,3487
 matogrosso = subset(deter, (lon <= -50.2244 & lon >= -61.6325) & (deter$lat <= -7.3487 & deter$lat >= -18.0418))
 matogrosso
@@ -18,22 +20,44 @@ p <- p + geom_point(data = matogrosso, aes(x = lon, y = lat, color="red", label=
 p <- p + labs(list(title = "Places related to an DETER alert", x = "Longitude", y="Latitude"))
 print(p)
 
-# the time stamps
+# convert dates to Date object needed 
 matogrosso$dates = strsplit(as.vector(matogrosso$dates), ';')
-dates = unlist(dates)
-
-
 matogrosso$dates = lapply(matogrosso$dates, function(x) lapply(x, function (x) as.Date(x, format="%Y-%m-%d")))
 
-
 # the filtered evi values
-evi = strsplit(as.vector(matogrosso$evi), ';')
-matogrosso$evi = as.numeric(unlist(evi))
+matogrosso$evi = strsplit(as.vector(matogrosso$evi), ';')
+matogrosso$evi = lapply(matogrosso$evi, function(x) lapply(x, function (x) as.numeric(x)))
 
 # the original evi values
-evio = strsplit(as.vector(matogrosso$evio), ';')
-matogrosso$evio = as.numeric(unlist(evio))
+matogrosso$evio = strsplit(as.vector(matogrosso$evio), ';')
+matogrosso$evio = lapply(matogrosso$evio, function(x) lapply(x, function (x) as.numeric(x)))
 
-mgts = ts(evi, frequency = 23)
+timeseriesEvio = ts(unlist((matogrosso$evio[1])), c(2000, 6), frequency=23)
+timeseriesEvi = ts(unlist((matogrosso$evi[1])), c(2000, 6), frequency=23)
 
+plot(timeseries)
 
+# get the registered date of an event related to the sample (dateevent) and start monitoring before this event
+dateevent = as.Date(as.vector(matogrosso$dateevent[1]))
+
+# function for converting from the registered DETER event to a c(period, cycle) structure
+dateToPeriodCycle = function(date){
+  period = as.numeric(format(date, "%Y"))
+  month = as.numeric(format(date, "%m"))
+  cycle = month * 2
+  c(period, cycle)
+}
+
+# function for converting from a numeric date to c(period, cycle) structure
+numericDateToPeriodCycle = function(numericDate, freq){
+  c(floor(numericDate), round((numericDate - floor(numericDate)) * freq) + 1)
+}
+
+timeAsPeriodCycle = dateToPeriodCycle(dateevent)
+
+# going back 5 measurements before the time of the event
+timeBeforeEvent = c(timeAsPeriodCycle[1], timeAsPeriodCycle[2] - 10)
+
+bfm = bfastmonitor(timeseries, start=timeBeforeEvent)
+bfm
+plot(bfm)
